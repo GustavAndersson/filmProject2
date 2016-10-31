04742097e8c9c9f56041816b815b1b3a381272e1
GustavAndersson <guustav.andersson@gmail.com>
2016-10-31 18:33:41 +0100

    Movie Register

---
+++ src/guitest/Filmer.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guitest;
+
+public class Filmer {
+
+        private int id;
+        private String titel;
+        private String genre;
+        private String regissör;
+        private String iMDb;
+        private String längd;
+
+        public Filmer(int Id, String Titel, String Genre, String Regissör, String IMDb, String Längd) {
+            this.id = Id;
+            this.titel = Titel;
+            this.genre = Genre;
+            this.regissör = Regissör;
+            this.iMDb = IMDb;
+            this.längd = Längd;
+        }
+
+        public int getId() {
+            return this.id;
+        }
+
+        public String getTitel() {
+            return this.titel;
+        }
+
+        public String getGenre() {
+            return this.genre;
+        }
+
+        public String getRegissör() {
+            return this.regissör;
+        }
+
+        public String getIMDb() {
+            return this.iMDb;
+        }
+
+        public String getLängd() {
+            return this.längd;
+        }
+    }
